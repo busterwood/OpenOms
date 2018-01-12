bc723266b1670722860f87cf9a4f52072e10d72b
BusterWood <chris.j.austin@gmail.com>
2018-01-12 15:30:40 +0000

    Some code

---
+++ Ordering/Class1.cs
@@ -0,0 +1,105 @@
+﻿using BusterWood.Goodies;
+using System;
+
+namespace OpenOms.Ordering
+{
+    /// <summary>A decision is made to trade</summary>
+    public class InvestmentDecision
+    {
+        public Guid<InvestmentDecision> InvestmentDecisionId { get; set; }
+        public Int32<User> DecisionMakerId { get; set; }
+        public Int32<User> InputterId { get; set; }
+        public DateTimeOffset DecisionTaken { get; set; }
+        public string Description { get; set; }
+
+        /// <summary>Does this investment roll to the next trade date if not fully traded on the first day?</summary>
+        public bool Roll { get; set; }
+        
+        /// <summary>Source of this investment, if it came from outside</summary>
+        public string SourceSystem { get; set; }
+
+        /// <summary>Any external ID associated with this <see cref="InvestmentDecision"/></summary>
+        public string ExternalId { get; set; }
+    }
+
+    /// <summary>A user of the system</summary>
+    public class User
+    {
+        public Int32<User> UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    /// <summary>A <see cref="InvestmentDecision"/> to trade a specific <see cref="SecurityId"/></summary>
+    public class Order
+    {
+        public Guid<Order> OrderId { get; set; }
+
+        public Guid<InvestmentDecision> InvestmentDecisionId { get; set; }
+
+        public Int32<Security> SecurityId { get; set; }
+
+        /// <summary>If set then this is a limit order, other wise a market order</summary>
+        public decimal? LimitPrice { get; set; }
+
+        public string Comments { get; set; }
+
+        /// <summary>ISO currency code</summary>
+        public string CurrencyCode { get; set; }
+
+        /// <summary>Any external ID associated with this <see cref="Order"/></summary>
+        public string ExternalId { get; set; }
+    }
+
+    public class ExecutionDirections
+    {
+        public Int32<Ems> EmsId { get; set; }
+        
+        /// <summary>How to execute</summary>
+        public int ExecutionStyle { get; set; }
+    }
+
+    public class Ems
+    {
+        public Int32<Ems> EmsId { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>Part of a an <see cref="Order"/> with a <see cref="Quantity"/> for a <see cref="Beneficiary"/></summary>
+    public class OrderLine
+    {
+        public Guid<Order> OrderId { get; set; }
+        public decimal Quantity { get; set; }
+        public Guid<Beneficiary> BeneficiaryId { get; set; }
+
+        //TODO: do we need this information here? seems likely this is only for booking?
+
+        /// <summary>Destination position that this trade effects</summary>
+        public Int32<Position> PositionId { get; set; }
+
+        /// <summary>ISO currency code of the intended position</summary>
+        public string PositionCurrencyCode { get; set; }
+
+        /// <summary>Any external ID associated with this <see cref="OrderLine"/></summary>
+        public string ExternalId { get; set; }
+
+    }
+
+    public class Position
+    {
+        public Int32<Position> PositionId { get; set; }
+    }
+
+    public class Security
+    {
+        public string Name { get; set; }
+
+    }
+
+    /// <summary>The fund, or some sub-part of a fund, than an <see cref="OrderLine"/> is for, e.g. book within a fund</summary>
+    public class Beneficiary
+    {
+        public Guid<Beneficiary> Id { get; set; }
+    }
+
+}
---
+++ Ordering/Class2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusterWood.Collections;
+using BusterWood.Goodies;
+
+namespace OpenOms.Ordering
+{
+    public class ComplianceChecker
+    {
+        //TODO: I don't like it, this is not a function
+        public ComplianceResult Check(InvestmentDecision investmentDecision)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ComplianceResult
+    {
+
+    }
+
+    public class ExecutionOrderCreator
+    {
+        public UniqueList<ExecutionOrder> CreateExecutionOrders(IEnumerable<Order> orders)
+        {
+            throw new NotImplementedException(); // merge order lines across orders where restrictions are compatible
+        }
+    }
+
+    /// <summary>An order to be execution in the EMS</summary>
+    public class ExecutionOrder
+    {
+        public Int32<Security> SecurityId { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        /// <summary>Is crossing EMS?</summary>
+        public bool CrossingAllowed { get; set; }
+
+        /// <summary>Can this be mixed in the EMS?</summary>
+        public bool MixingAllowed { get; set; }
+
+        /// <summary>Can this be merged in the EMS?</summary>
+        public bool MergingAllowed { get; set; }
+
+        /// <summary>The counterparties that must be traded with, empty list mean any counterparty is okay</summary>
+        public UniqueList<Int32<Counterparty>> CounterPartyWhiteList { get; set; }
+
+        /// <summary>The counterparties that cannot be traded with, empty list mean any counterparty is okay</summary>
+        public UniqueList<Int32<Counterparty>> CounterPartyBlackList { get; set; }
+    }
+
+    public class Counterparty
+    {
+
+    }
+}
